493426cd618ef6a9135d8d821b4d09674c053b32
Ivan Galaviz <ivanovishado@gmail.com>
2025-03-30 21:50:10 -0600

    Remove /blog from URL

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const { DateTime } = require("luxon");
+const path = require("path");
 
 module.exports = function(eleventyConfig) {
 
@@ -8,6 +9,18 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+
+  // Add a collection for blog posts with full file name as URL
+  eleventyConfig.addCollection("post", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("src/blog/*.md").map(item => {
+      // Extract the file name without extension (e.g., "2021-06-15-my-fifth-article copy")
+      const fileName = path.basename(item.inputPath, ".md");
+      // Use the full file name as the slug
+      item.outputPath = `public/${fileName}/index.html`;
+      item.url = `/${fileName}/`;
+      return item;
+    });
   });
 
   return {
